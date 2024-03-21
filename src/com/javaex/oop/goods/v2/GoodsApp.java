7bb9730029f1ef636be0764d47b82ed80f926d23
한대희 <eogml01023@gmail.com>
2024-03-21 16:59:00 +0900

    20240321

---
+++ src/com/javaex/oop/goods/v2/GoodsApp.java
@@ -0,0 +1,28 @@
+package com.javaex.oop.goods.v2;
+
+public class GoodsApp {
+
+	public static void main(String[] args) {
+		Goods camera = new Goods();
+		
+		camera.name = "nikon";
+		camera.price = 400_000;
+		
+		Goods notebook = new Goods();
+		
+		notebook.name = "LG그램";
+		notebook.price = 900_000;
+		
+		Goods cup = new Goods();
+		
+		cup.name = "머그컵";
+		cup.price = 2_000;
+		
+		
+		//	상품 정보 출력
+		System.out.printf("%s, %,d원%n", camera.name, camera.price);
+		System.out.printf("%s, %,d원%n", notebook.name, notebook.price);
+		System.out.printf("%s, %,d원%n", cup.name, cup.price);
+	}
+
+}
