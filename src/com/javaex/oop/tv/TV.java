a69f9c83371a065d2be8d721c8ac1c4df1746e1a
한대희 <eogml01023@gmail.com>
2024-03-26 18:27:10 +0900

    xxx

--- src/com/javaex/oop/tv/TV.java
+++ src/com/javaex/oop/tv/TV.java
@@ -1,13 +1,17 @@
 package com.javaex.oop.tv;
 
 class TV {
-    private int channel;
+    //상수 
+	//해당 클래스에선 설정하지 않았음
+	//private static final ~~
+	
+	//필드
+	private int channel;
     private int volume;
     private boolean power;
 
     // 생성자
-
-    public TV(int channel, int volume, boolean power) {
+    public TV(int channel, int volume, boolean power) { //A
         this.channel = channel;
         this.volume = volume;
         this.power = power;
@@ -22,7 +26,7 @@ class TV {
         return volume;
     }
 
-    public boolean isPower() {
+    public boolean isPower() { 
         return power;
     }
 
@@ -30,46 +34,67 @@ class TV {
     public void power(boolean on) {
         this.power = on;
     }
-
+    
+    //채널 변경 메서드
     public void channel(int channel) {
         if (channel >= 1 && channel <= 255) {
             this.channel = channel;
         }
     }
 
+    //채널 변경 메서드(오버로딩)
     public void channel(boolean up) {
+        if (up) {	//채널 올림 
+            if (channel < 255) { //최대 채널
+                channel++;			// 채널 올림
+            }
+        } else if (channel > 1) { //최소 채널
+               channel--;			// 채널 내림
+            }
+        }
+    
+
+    public void volume(int volume) { 
+        if (volume >= 0 && volume <= 100) { //볼륨 제한
+            this.volume = volume;			//볼륨 설정
+        }
+    }
+
+    public void volume(boolean up) { 
         if (up) {
-            if (channel < 255) {
-                channel++;
+            if (volume < 100) { //최대 볼륨
+                volume++;		//볼륨 올림
             }
-        } else {
-            if (channel > 1) {
-                channel--;
-            }
-        }
-    }
-
-    public void volume(int volume) {
-        if (volume >= 0 && volume <= 100) {
-            this.volume = volume;
-        }
-    }
-
-    public void volume(boolean up) {
-        if (up) {
-            if (volume < 100) {
-                volume++;
-            }
-        } else {
-            if (volume > 0) {
-                volume--;
+        } else if (volume > 0) { //최소 볼륨
+                volume--;		 //볼륨 내림
             }
         }
-    }
+    
 
-    public void status() {
-        System.out.println("TV 채널: " + channel);
-        System.out.println("TV 볼륨: " + volume);
-        System.out.println("TV 전원: " + (power ? "ON" : "OFF"));
+    public void status() { 
+    	if (power == true) {
+    		System.out.println("TV 채널: " + channel);
+    	    System.out.println("TV 볼륨: " + volume);
+    	    System.out.println("TV 전원: " + (power ? "ON" : "OFF"));
+    	} else {
+    		System.out.println("전원 off 상태표시 안함");
+    	}
+       
     }
+    
+    public void status(int tvnum) {
+    	if (tvnum==1 && power == true) {
+    		System.out.println("TV 채널: " + channel);
+            System.out.println("TV 볼륨: " + volume);
+            System.out.println("TV 전원: " + (power ? "ON" : "OFF"));
+    	}
+    	if (tvnum==2 && power == true) {
+    		System.out.println("TV2 채널: " + channel);
+            System.out.println("TV2 볼륨: " + volume);
+            System.out.println("TV2 전원: " + (power ? "ON" : "OFF"));
+    	} else {
+    		System.out.println("전원 off 상태표시 안함");
+    	}
+    }
+       
 }
--- src/com/javaex/oop/tv/TVApp.java
+++ src/com/javaex/oop/tv/TVApp.java
@@ -3,12 +3,12 @@ package com.javaex.oop.tv;
 public class TVApp {
 
 	public static void main(String[] args) {
-		TV tv = new TV (7, 20, false);
+		TV tv = new TV (7, 20, false); //A
 		
-		tv.status();
+		tv.status(); 
 		
 		tv.power(true);
-		tv.volume(120);
+		tv.volume(120); 
 		tv.status();
 		
 		tv.volume(false);
@@ -24,6 +24,29 @@ public class TVApp {
 		
 		tv.power(false);
 		tv.status();
+		
+		System.out.println("---------------------------------------");
+		TV tv2 = new TV (4, 10, true); 
+		
+		tv2.status(2); 
+		
+		tv2.power(true);
+		tv2.volume(60); 
+		tv2.status(2);
+		
+		tv2.volume(false);
+		tv2.status(2);
+		
+		tv2.channel(0);
+		tv2.status(2);
+		
+		tv2.channel (true);
+		tv2.channel (true);
+		tv2.channel (true);
+		tv2.status(2);
+		
+		tv2.power(false);
+		tv2.status(2);
 		
 	}
 
